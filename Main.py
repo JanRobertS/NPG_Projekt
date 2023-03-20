f850d5d4b0ff596c5aa366eb51ae9ec7bf53ba59
siata103 <43496848+siata103@users.noreply.github.com>
2023-03-20 21:05:55 +0100

    Update Main.py
    chyba wszystko

--- Main.py
+++ Main.py
@@ -32,10 +32,33 @@ def read_note():
     if os.path.exists(filename):
         with open(filename) as f:
             note = f.read()  # odczytaj notatkę z pliku
-        print(note)  # wyświetl notatkę
+        #print(note)  # wyświetl notatkę usunięte żeby funkcja była kompatybilna z send_email()
+        return note
     else:
         print("\nNotatka o takim tytule nie istnieje.\n")
 
+def send_note():
+    # hasło: mquoianwvzurwhyj
+    email_sender = 'testmailnpg123@gmail.com'
+    email_password = 'mquoianwvzurwhyj'
+    email_reciver = input('podaj adres email')
+
+    subject = title
+    body = read_note()
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_reciver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_reciver, em.as_string())
+
+        
 # odczytaj istniejące notatki z folderu notes
 for filename in os.listdir(notes_dir):
     if filename.endswith(".txt"):
@@ -47,6 +70,7 @@ while True:
     print("1. Utworzyć notatkę")
     print("2. Odczytać notatkę")
     print("3. Wyjść z programu")
+    print("4. Wyślij notatkę")
     choice = input("Wybierz opcję: ")
     if choice == "1":
         save_note()
@@ -54,5 +78,7 @@ while True:
         read_note()
     elif choice == "3":
         break
+    elif choice == "4":
+        send_note()
     else:
         print("\nNiepoprawny wybór. Spróbuj jeszcze raz.\n")
