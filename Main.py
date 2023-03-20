321645bc504ea8b8796fb643f17f46ad318fcb50
Igor Siata <igor.siata33@gmail.com>
2023-03-20 22:01:41 +0100

    naprawiono dwukrotne wyswietlanie notatki i dodano komentarze

--- Main.py
+++ Main.py
@@ -35,13 +35,13 @@ def read_note():
     if os.path.exists(filename):
         with open(filename) as f:
             note = f.read()  # odczytaj notatkę z pliku
-        print(note)  # wyświetl notatkę usunięte żeby funkcja była kompatybilna z send_email()
-        return note
+        return note #zwraca notatkę
     else:
         print("\nNotatka o takim tytule nie istnieje.\n")
 
 def send_note():
     # hasło: mquoianwvzurwhyj
+    #dane do wysyłania emaila
     email_sender = 'testmailnpg123@gmail.com'
     email_password = 'mquoianwvzurwhyj'
     email_reciver = input('\nPodaj adres email: ')
@@ -57,6 +57,7 @@ def send_note():
 
     context = ssl.create_default_context()
 
+    #wysyła email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_reciver, em.as_string())
